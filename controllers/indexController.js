710968ab84e7de742735ab6e46ff72a0a24bc07b
suix0 <112095627+suix0@users.noreply.github.com>
2025-03-06 12:40:58 +0800

    feat: added views for register form

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,9 @@
 const asyncHandler = require("express-async-handler");
 
 exports.getLandingPage = (req, res) => {
-  res.render("landingPage.ejs");
+  res.render("landingPage");
+};
+
+exports.getRegisterForm = (req, res) => {
+  res.render("register");
 };
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,5 +4,7 @@ const indexController = require("../controllers/indexController");
 const indexRouter = Router();
 
 indexRouter.get("/", indexController.getLandingPage);
+indexRouter.get("/register", indexController.getRegisterForm);
 
+indexRouter.post("/register", indexController.postRegisterForm);
 module.exports = indexRouter;
