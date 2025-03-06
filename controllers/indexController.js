8a0829544f0e00cc1031f420e64c9178d6b32a5b
suix0 <112095627+suix0@users.noreply.github.com>
2025-03-06 13:18:59 +0800

    feat: added password confirmation with validation in registration

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,34 @@
 const asyncHandler = require("express-async-handler");
+const db = require("../db/query");
+const { body, validationResult } = require("express-validator");
+
+// Custom validator to check if username already exists in our database
+
+const validateUser = [
+  body("firstname").trim().notEmpty().withMessage("First name can't be empty."),
+  body("lastname").trim().notEmpty().withMessage("Last name can't be empty."),
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage("Username can't be empty.")
+    .custom(async (value) => {
+      const dbUser = await db.getUsername(username);
+      if (dbUser) {
+        throw new Error("This user already exists.");
+      }
+    }),
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("Password can't be empty.")
+    .isLength({ min: 8 })
+    .withMessage("Password must have a minimum of 8 characters"),
+  body("confirmPassword").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Confirm that your passwords match.");
+    }
+  }),
+];
 
 exports.getLandingPage = (req, res) => {
   res.render("landingPage");
@@ -7,3 +37,14 @@ exports.getLandingPage = (req, res) => {
 exports.getRegisterForm = (req, res) => {
   res.render("register");
 };
+
+exports.postRegisterForm = [
+  validateUser,
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render("register", { error: errors.array() });
+    }
+    res.send(req.body);
+  },
+];
---
+++ db/query.js
@@ -0,0 +1,9 @@
+const db = require("./pool");
+
+// GET: Find a specific username if it exists
+exports.getUsername = async (username) => {
+  const { rows } = await db.query("SELECT * FROM users WHERE user_id = $1", [
+    username,
+  ]);
+  return rows[0];
+};
